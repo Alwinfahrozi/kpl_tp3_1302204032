00c35f9d9fabf92ddb4a5fbe9aa514268f6f11d5
Alwinfahrozi <alwin.marbun123@gmail.com>
2025-03-03 06:45:42 +0800

    tambahan fitur GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,20 @@
-﻿namespace kpl_tp3_1302204032
+﻿namespace tp3_1302204032
 {
     partial class Form1
     {
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label outputLabel;
+
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,19 +25,51 @@
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
 
+            this.SuspendLayout();
+
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(20, 20);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(200, 20);
+
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(20, 60);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.Text = "Submit";
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(20, 100);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(120, 20);
+            this.outputLabel.Text = "Output akan muncul di sini";
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(350, 200);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form Input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
         #endregion
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,19 @@
-namespace kpl_tp3_1302204032
+using System;
+using System.Windows.Forms;
+
+namespace tp3_1302204032
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string input = inputTextBox.Text;
+            outputLabel.Text = $"Halo {input}";
         }
     }
 }
